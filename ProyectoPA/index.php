963c8ca6bdee38d8b1fc66aa5620d1a5d6a09aea
Aescfue <aescfue@protonmail.com>
2021-12-04 17:55:44 +0100

    Subida carpeta con proyecto php

---
+++ ProyectoPA/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>
+            <?php
+            echo "Hoy es " . date('d/m/Y');
+            echo '</br>Son las '.date('H:i');
+            ?>
+        </p>
+    </body>
+</html>
